f2a8a3a7265d6dd999c4ae6ce5b96243574e276d
leomoraesguitar <123950901+leomoraesguitar@users.noreply.github.com>
2024-09-27 21:56:12 -0300

    10 assets

--- criar_flet.py
+++ criar_flet.py
@@ -9,7 +9,7 @@ def create(base_path):
         # "src/controllers",
         # "src/models",
         # "src/views",
-        # "tests",
+        "assets",
         '.github/workflows'
     ]
     
@@ -125,20 +125,29 @@ class Resize:
         self.pw.update()
 
 
-class Saida(ft.Column):
-    def __init__(self, height=150, page = None):
-        super().__init__()
+
+class Saida:
+    def __init__(self,  page = None):
         self.page = page
-        self._height = height
-        self.saidad = ft.Text('', selectable=True)
-        self.controls.append(ft.Container(ft.ListView([self.saidad],auto_scroll = True, height=self._height,  ),bgcolor='white,0.08' ))
+        self.snac = ft.SnackBar(
+                    content = ft.Text('', selectable=True, color=ft.colors.BROWN_100),
+                    open=True,
+                    bgcolor=ft.colors.GREY_900,
+                )
+ 
+    
     def pprint(self, *texto):
         for i in list(texto):
-            self.saidad.value += f'{{i}}\\n'
+            self.snac.content.value = f'{{i}}'
+            self.page.open(
+                self.snac
+            )            
         try:
             self.page.update()
         except:
             pass
+
+
 
     @property
     def height(self):
@@ -157,7 +166,9 @@ class Saida(ft.Column):
 
 
 class ClassName(ft.Column):
-    def __init__(self):
+    def __init__(self,page, pprint):
+        self.page = page
+        self.pprint = pprint
         super().__init__()
         self.controls = [ft.Text('Meu ovo')]
 
@@ -177,31 +188,114 @@ def main(page: ft.Page):
                   }},
         ),
         use_material3 = True,
-        # color_scheme=ft.ColorScheme(
-        #     primary=ft.colors.BLUE,       # Cor principal
-        #     secondary=ft.colors.GREEN,    # Cor secundária
-        #     error=ft.colors.RED,          # Cor de erro
-        #     background=ft.colors.WHITE,   # Cor de fundo
-        #     surface=ft.colors.LIGHT_BLUE  # Cor de superfície
-        # )
+        color_scheme=ft.ColorScheme(
+            primary = ft.colors.WHITE70, # cor fundo filledbutton e cor texto outlinedbutton
+            on_primary = ft.colors.BLACK, #cor texto filledbutton e cor da bolinha do swicth com True
+            secondary_container = ft.colors.GREY_700, # cor de fundo filledtonalbutton
+            on_secondary_container = ft.colors.WHITE, # cor de tecto filledtonalbutton
+            outline = ft.colors.GREY_600, #cor de borda do outliedbutton
+            shadow = ft.colors.BLUE_300, # cor das sombras
+            on_surface_variant = ft.colors.WHITE, #cor dos labela
+            surface_variant = ft.colors.GREY_600, #cor do slider e cor de fundo do texfield
+            primary_container = ft.colors.WHITE70, #cor da bolinha do switch
+            on_surface = ft.colors.WHITE, #cor HOVERED do checkbox
+
+        ),
         divider_theme=ft.DividerTheme(
             color=ft.colors.with_opacity(0.5, ft.colors.GREY_800),      # Cor do divisor
             thickness=1,               # Espessura da linha divisória
             leading_indent=1,                 # Recuo inicial
             trailing_indent=1              # Recuo final
         ),
+        text_theme=ft .TextTheme(
+            title_large=ft.TextStyle(
+                size = 20,
+                weight=ft.FontWeight.W_800,
+            )
+        ),
+        slider_theme=ft.SliderTheme(
+            thumb_color = ft.colors.GREY_700,
+        ),
+        switch_theme= ft.SwitchTheme(
+            thumb_color = {{
+                ft.ControlState.DEFAULT:ft.colors.GREY_400,
+                ft.ControlState.HOVERED:None,
+                ft.ControlState.SELECTED:ft.colors.GREY_300,
 
-    
+            }},
+            track_color = {{
+                ft.ControlState.DEFAULT:ft.colors.GREY_700,
+                ft.ControlState.HOVERED:ft.colors.GREY_700,
+            }},
+            overlay_color = {{
+                ft.ControlState.DEFAULT:ft.colors.TRANSPARENT,
+                ft.ControlState.HOVERED:ft.colors.TRANSPARENT,
+            }},
+            track_outline_color= {{
+                ft.ControlState.DEFAULT:ft.colors.WHITE,
+                ft.ControlState.HOVERED:ft.colors.WHITE,
+            }},
+            track_outline_width= {{
+                ft.ControlState.DEFAULT:0,
+                ft.ControlState.HOVERED:0
+            }},
+        ),
+        checkbox_theme = ft.CheckboxTheme(
+            overlay_color = {{
+                ft.ControlState.DEFAULT:ft.colors.TRANSPARENT,
+                ft.ControlState.HOVERED:ft.colors.TRANSPARENT,
+            }},                
+        ),
     )    
 
-    ConfirmarSaida(page  = page)
-    Resize(page)
-    p = ClassName()
-    page.add(p,saida)
+    page.navigation_bar = ft.CupertinoNavigationBar(
+            bgcolor= ft.colors.BLACK38,
+            inactive_color=ft.colors.GREY,
+            active_color=ft.colors.GREEN_800,
+            on_change=lambda e: print("Selected tab:", e.control.selected_index),
+            destinations=[
+                ft.NavigationBarDestination(icon=ft.icons.EXPLORE, label="Explore"),
+                ft.NavigationBarDestination(icon=ft.icons.COMMUTE, label="Commute"),
+                ft.NavigationBarDestination(
+                    icon=ft.icons.BOOKMARK_BORDER,
+                    selected_icon=ft.icons.BOOKMARK,
+                    label="Explore",
+                ),
+            ]
+        )
+    
+    page.appbar = ft.AppBar(
+        actions = [ft.Container(ft.Text('meu pau')) for i in range(5)],
+        title=ft.Text(
+            value = 'Guerra de Clans', 
+            weight='BOLD', 
+            color=ft.colors.GREEN_600,
+            style=ft.TextStyle(
+                shadow = ft.BoxShadow(
+                    blur_radius = 300,
+                    # blur_style = ft.ShadowBlurStyle.OUTER,
+                    color = ft.colors.WHITE
+                ),                
+            )
+            ),
+        shadow_color=ft.colors.BLUE,
+        elevation=8,
+        toolbar_height = 30,
+        bgcolor=ft.colors.BLACK38,
+        automatically_imply_leading=False,
+    )     
 
-if __name__ == '__main__': 
+
+
+
     saida = Saida()
     print = saida.pprint 
+    ConfirmarSaida()
+    Resize(page)
+    p = ClassName(page  = page, pprint = saida.pprint)
+    page.add(p)
+
+if __name__ == '__main__': 
     ft.app(target=main)
 
 """
